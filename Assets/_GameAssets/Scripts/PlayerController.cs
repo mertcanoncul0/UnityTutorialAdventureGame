04dbca87d006cad5e8e1a215629ab183cca3efc3
Mertcan Öncül <mertcanoncul@icloud.com>
2025-06-28 05:47:43 +0300

    Make your game frame rate independent

--- Assets/_GameAssets/Scripts/PlayerController.cs
+++ Assets/_GameAssets/Scripts/PlayerController.cs
@@ -9,13 +9,15 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         MoveAction.Enable();
+        // QualitySettings.vSyncCount = 0;
+        // Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 move = MoveAction.ReadValue<Vector2>();
-        Vector2 position = (Vector2)transform.position + move * 0.1f;
+        Vector2 position = (Vector2)transform.position + move * (3.0f * Time.deltaTime);
         transform.position = position;
     }
 }
