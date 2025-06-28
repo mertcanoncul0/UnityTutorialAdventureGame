6d33298b0c4864d85516b959b055dbac55532f45
Mertcan Öncül <mertcanoncul@icloud.com>
2025-06-28 05:41:06 +0300

    Create Input Actions for player character movement

--- Assets/_GameAssets/Scripts/PlayerController.cs
+++ Assets/_GameAssets/Scripts/PlayerController.cs
@@ -1,41 +1,21 @@
 using UnityEngine.InputSystem;
-using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public InputAction MoveAction;
+
     void Start()
     {
-
+        MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = 0.0f;
-        float vertical = 0.0f;
-
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal = -0.3f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = 0.3f;
-        }
-        else if (Keyboard.current.upArrowKey.isPressed)
-        {
-            vertical = 0.3f;
-        }
-        else if (Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical = -0.3f;
-        }
-
-        Vector2 position = transform.position;
-        position.x += horizontal * 0.1f;
-        position.y += vertical * 0.1f;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Vector2 position = (Vector2)transform.position + move * 0.1f;
         transform.position = position;
     }
 }
